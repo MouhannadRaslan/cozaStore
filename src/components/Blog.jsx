b556b90ad4cb0afe413c598893a63677059d09aa
MouhannadRaslan <raslanmohanad11@gmail.com>
2025-02-24 03:44:04 +0300

    end all pages

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,79 @@
 import Footer from "./Footer";
+import BlogStyle from './Blog.module.css';
 
 function Blog(){
     return(
         <>
+            <div className={BlogStyle.blog}>
+                <div className={BlogStyle.blogBanner}>
+                    <h1>Blog</h1>
+                </div>
+                <div className={BlogStyle.blogContent}>
+                        <div className={BlogStyle.leftSection}>
+                            {/* first blog */}
+                            <div className={BlogStyle.firstImgContainer}>
+                                <div className={BlogStyle.firstImg}></div>
+                            </div>
+                            <div className={BlogStyle.blogText}>
+                                <h1>8 Inspiring Ways to Wear Dresses in the Winter</h1>
+                                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce eget dictum tortor. Donec dictum vitae sapien eu varius</p>
+                            </div>
+                            {/* second blog */}
+                            <div className={BlogStyle.secondImgContainer}>
+                                <div className={BlogStyle.secondImg}></div>
+                            </div>
+                            <div className={BlogStyle.blogText}>
+                                <h1>8 Inspiring Ways to Wear Dresses in the Winter</h1>
+                                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce eget dictum tortor. Donec dictum vitae sapien eu varius</p>
+                            </div>
+                            {/* third blog */}
+                            <div className={BlogStyle.thirdImgContainer}>
+                                <div className={BlogStyle.thirdImg}></div>
+                            </div>
+                            <div className={BlogStyle.blogText}>
+                                <h1>8 Inspiring Ways to Wear Dresses in the Winter</h1>
+                                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce eget dictum tortor. Donec dictum vitae sapien eu varius</p>
+                            </div>
+                            <div className={BlogStyle.circle}>
+                                    <span className={BlogStyle.one}>1</span>
+                                    <span className={BlogStyle.two}>2</span>
+                            </div>
+                        </div>
+                        <div className={BlogStyle.rightSection}>
+                            <div className={BlogStyle.searchInput}>
+                            <input type="text" className={BlogStyle.search} placeholder="Search" />
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                            </div>
+                            <div className={BlogStyle.categories}>
+                                <h1>Categories</h1>
+                                <a href="">Fashion</a>
+                                <a href="">Beauty</a>
+                                <a href="">Street Style</a>
+                                <a href="">Life Style</a>
+                                <a href="">DIY & Crafts</a>
+                            </div>
+                            <div className={BlogStyle.archive}>
+                                <h1>Archive</h1>
+                                <a href="">July 2018 <span>(9)</span></a>
+                                <a href="">June 2018 <span>(39)</span></a>
+                                <a href="">May 2018 <span>(29)</span></a>
+                                <a href="">April 2018 <span>(35)</span></a>
+                                <a href="">March 2018 <span>(22)</span></a>
+                                <a href="">February 2018 <span>(32)</span></a>
+                                <a href="">January 2018 <span>(21)</span></a>
+                                <a href="">December 2017 <span>(26)</span></a>
+                            </div>
+                            <div className={BlogStyle.tags}>
+                                <h1>Tags</h1>
+                                <a href="">Fashion</a>
+                                <a href="">Lifestyle</a>
+                                <a href="">Denim</a> <br />
+                                <a href="">Streetstyle</a>
+                                <a href="">Crafts</a>
+                            </div>
+                        </div>
+                </div>
+            </div>
             <Footer/>
         </>
     )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,45 @@
 import Footer from "./Footer";
+import ContactStyle from "./Contact.module.css";
 
-function Contact(){
-    return(
-        <>
-            <Footer/>
-        </>
-    )
+function Contact() {
+  return (
+    <>
+      <div className={ContactStyle.contact}>
+        <div className={ContactStyle.contactBanner}>
+          <h1>Contact</h1>
+        </div>
+        <div className={ContactStyle.contactBoxes}>
+          <div className={ContactStyle.firstBox}>
+            <h3>Send Us A Message</h3>
+            <form action="">
+              <input type="email" placeholder="Your Email Address" />
+              <i class="fa-regular fa-envelope"></i>
+              <textarea
+                name=""
+                id=""
+                placeholder="How Can We Help ?"
+                rows={12}
+              ></textarea>
+              <button type="submit">SUBMIT</button>
+            </form>
+          </div>
+
+          <div className={ContactStyle.secondBox}>
+            <i class="fa-solid fa-location-dot"></i>
+            <h3>Address</h3>
+            <p>Coza Store Center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
+            <i class="fa-solid fa-phone-flip"></i>
+            <h3>Lets Talk</h3>
+            <p style={{color:'#717fe0'}}>+1 800 1236879</p>
+            <i class="fa-regular fa-envelope"></i>
+            <h3>Sale Support</h3>
+            <p style={{color:'#717fe0'}}>contact@example.com</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Contact;
